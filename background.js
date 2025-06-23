421334c049864e82d9c8f51f3f936af9346b9f31
Prashant Khadka Magar <pkhadka-magar@myseneca.ca>
2025-06-23 14:30:25 -0400

    add completed frontend

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
-chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
-//   console.log(data);
+// chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
+// //   console.log(data);
 
-  chrome.storage.local.set({title: data.title});
-  sendResponse({ success: true });
-  return true;
-});
+//   chrome.storage.local.set({title: data.title});
+//   sendResponse({ success: true });
+//   return true;
+// });
+
+
